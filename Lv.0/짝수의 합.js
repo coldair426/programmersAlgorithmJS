508b9fd4a1743030738fbe3535b512f46ab18d00
chanki <coldair426@gmail.com>
2022-12-16 00:57:11 +0900

    Lv.0 짝수의합(수정)

--- Lv.0/짝수의 합.js
+++ Lv.0/짝수의 합.js
@@ -1,8 +1,6 @@
 function solution(n) {
-  let answer = 0;
-  const evenNumber = new Array(n).fill(0).map((v, i) => (v = i + 1));
-  evenNumber.forEach((i) => {
-    i % 2 === 0 ? (answer += i) : answer;
-  });
-  return answer;
+  return new Array(n)
+    .fill(0)
+    .map((v, i) => (v = i + 1))
+    .reduce((acc, current) => (current % 2 === 0 ? (acc += current) : acc), 0);
 }
